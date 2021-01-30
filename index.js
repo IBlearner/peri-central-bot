4796a566ef4412d42332ef88b110fa69064cfbe0
iblearner <kienvi96@hotmail.com>
2021-01-30 16:48:19 +1000

    Added some puppet roster code to begin scraping for all staff member names.

--- index.js
+++ index.js
@@ -2,11 +2,34 @@ const puppeteer = require('puppeteer');
 
 const user = "kienvi"
 const password = "periperi"
+const adiiUser = "kienvi"
+const adiiPassword = "KLSV110411"
+const rosterURL = "https://see.adiinsights.com/shifts/?unit=12660&date=2021-01-30"
 const accManURL = "https://pericentral.worldmanager.com/admin/ctrl?page=accounts%2Fprofile"
+
+const CheckStaff = async (page) => {
+    await page.goto(rosterURL)
+    try {
+        await page.waitForSelector('input[name=username]');
+        await page.$eval('input[name=username]', (el, x) => {el.value = x}, adiiUser)
+    
+        await page.waitForSelector('input[name=password]');
+        await page.$eval('input[name=password]', (el, x) => {el.value = x}, adiiPassword)
+    
+        await page.click("button[id='sign-in']")
+
+        console.log(`Username (${adiiUser}) and password has been submitted to Adii`)
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 const Puppet = async () => {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
+
+    await CheckStaff(page)
+
     await page.goto(accManURL);
 
     const usernameElement = "input[name=username]"
@@ -19,7 +42,7 @@ const Puppet = async () => {
 
     await page.click("button[name=loginbutton]")
 
-    console.log(`Attempting to log in using ${user} details`)
+    console.log(`Attempting to log into ${user}'s details on peri central`)
 
     //waiting for the next page to load
     await page.waitForSelector("tr")
