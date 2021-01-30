978470e6e65b68453917a8f481189948f94012ea
iblearner <kienvi96@hotmail.com>
2021-01-30 16:22:44 +1000

    Added basic code to input user name, password and click the login button.

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://example.com');
+const user = "kienvi"
+const password = "periperi"
+const accManURL = "https://pericentral.worldmanager.com/admin/ctrl?page=accounts%2Fprofile"
+
+const Puppet = async () => {
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.goto(accManURL);
+
+    const usernameElement = "input[name=username]"
+    await page.waitForSelector(usernameElement)
+    await page.$eval(usernameElement, (el, x) => {el.value = x}, user)
+
+    const passwordElement = "input[name=password]"
+    await page.waitForSelector(passwordElement)
+    await page.$eval(passwordElement, (el, x) => {el.value = x}, password)
+
+    await page.click("button[name=loginbutton]")
+
+    console.log(`Attempting to log in using ${user} details`)
 //   await page.screenshot({path: 'example.png'});
 
 //   await browser.close();
-})();
+}
+
+Puppet()
