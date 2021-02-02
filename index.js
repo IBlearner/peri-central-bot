b3010dff5fd247650a6438737b7bad3b6c7940a3
iblearner <kienvi96@hotmail.com>
2021-02-02 18:02:06 +1000

    Puppeteer accurately scrapes all names from the roster and appends it to a txt file. May need to change if more data is required to be logged such as password.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const fs = require("fs")
 
 const user = "kienvi"
 const password = "periperi"
@@ -19,8 +20,30 @@ const CheckStaff = async (page) => {
         await page.click("button[id='sign-in']")
 
         console.log(`Username (${adiiUser}) and password has been submitted to Adii`)
+
+        await page.waitForSelector("tbody")
+
+        const staff = await page.$$eval("tbody > tr", a => a.map(single => {
+            const name = single.querySelector("th").innerText
+            return name
+        }))
+        console.log(staff)
+
+        await WriteNameToFile(staff)
+        
     } catch (error) {
         console.log(error)
+    }
+}
+
+const WriteNameToFile = async (x) => {
+    const fileName = "names.txt"
+    const dir = `${__dirname}/${fileName}`
+    fs.openSync(dir, 'w')
+    for (let i = 0; i < x.length; i++) {
+        var splitName = x[i].split(" ")
+        var firstName = splitName[0]
+        fs.appendFileSync(dir, `${firstName} `)
     }
 }
 
@@ -29,6 +52,8 @@ const Puppet = async () => {
     const page = await browser.newPage();
 
     await CheckStaff(page)
+
+    return
 
     await page.goto(accManURL);
 
