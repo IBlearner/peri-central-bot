6abf0791f54d68aaee705cea1b37aedac8df91fb
iblearner <kienvi96@hotmail.com>
2021-02-04 16:49:01 +1000

    App only overrides the txt file if a third arg (anything) is supplied. Needs tweaking of the data writing to accurately identify staff's usernames.

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const adiiUser = "kienvi"
 const adiiPassword = "KLSV110411"
 const rosterURL = "https://see.adiinsights.com/shifts/?unit=12660&date=2021-01-30"
 const accManURL = "https://pericentral.worldmanager.com/admin/ctrl?page=accounts%2Fprofile"
+const accManLogoutURL = "https://pericentral.worldmanager.com/admin/index.php?logout=true"
+
+const fileName = "names.txt"
+const dir = `${__dirname}/${fileName}`
+
+const Sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 //responsible for making the puppet log into adii and getting staff names
 const CheckStaff = async (page) => {
@@ -39,30 +47,22 @@ const CheckStaff = async (page) => {
 
 //opening a txt file if one doesn't already exist. appends an array of names scraped from adii
 const WriteNameToFile = async (x) => {
-    const fileName = "names.txt"
-    const dir = `${__dirname}/${fileName}`
     fs.openSync(dir, 'w')
     for (let i = 0; i < x.length; i++) {
+        var combinedName = x[i].replace(" ", "")
         var splitName = x[i].split(" ")
         var firstName = splitName[0]
+        fs.appendFileSync(dir, `${combinedName} `)
         fs.appendFileSync(dir, `${firstName} `)
     }
 }
 
-const Puppet = async () => {
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-
-    //works, txt file already exists though just skipping this step.
-    //await CheckStaff(page)
-
-    return
-
-    await page.goto(accManURL);
+const LoginAttempt = async (page, staffName) => {
+    await page.goto(accManURL)
 
     const usernameElement = "input[name=username]"
     await page.waitForSelector(usernameElement)
-    await page.$eval(usernameElement, (el, x) => {el.value = x}, user)
+    await page.$eval(usernameElement, (el, x) => {el.value = x}, staffName)
 
     const passwordElement = "input[name=password]"
     await page.waitForSelector(passwordElement)
@@ -70,7 +70,7 @@ const Puppet = async () => {
 
     await page.click("button[name=loginbutton]")
 
-    console.log(`Attempting to log into ${user}'s details on peri central`)
+    console.log(`Attempting to log into ${staffName}'s details on peri central`)
 
     //waiting for the next page to load
     await page.waitForSelector("tr")
@@ -80,9 +80,29 @@ const Puppet = async () => {
     try {
         headerCheck = await page.$eval("span[class='header-title']", el => el.innerHTML)
         console.log("Successfully logged in")
+        await page.goto(accManLogoutURL)
+        return
     } catch (error) {
         console.log("Unsuccessful login attempt")
     }
+}
+
+const Puppet = async () => {
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+
+    //works, txt file already exists though just skipping this step.
+    if (typeof process.argv[2] !== "undefined") {
+        await CheckStaff(page)
+    }
+
+    const staffNames = fs.readFileSync(dir, {encoding:'utf8', flag:'r'})
+    const staffNamesArray = staffNames.split(" ")
+    for (let i = 0; i < staffNamesArray.length; i++) {
+        await LoginAttempt(page, staffNamesArray[i])
+        await Sleep(5000)
+    }
+
 //   await page.screenshot({path: 'example.png'});
 
 //   await browser.close();
